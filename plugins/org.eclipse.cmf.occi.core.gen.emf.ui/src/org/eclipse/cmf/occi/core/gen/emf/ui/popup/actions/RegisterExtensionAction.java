84f24496bb2cf15e9237b2e2df4e051bd54d2eb9
fzalila <faiez.zalila@gmail.com>
2017-04-19 18:01:09 +0200

    Add the RegisterExtensionAction (temporary here) to be able to create extensions refering to other ones.

---
+++ plugins/org.eclipse.cmf.occi.core.gen.emf.ui/src/org/eclipse/cmf/occi/core/gen/emf/ui/popup/actions/RegisterExtensionAction.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2016 Obeo, Inria
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * - William Piers <william.piers@obeo.fr>
+ * - Philippe Merle <philippe.merle@inria.fr>
+ *******************************************************************************/
+package org.eclipse.cmf.occi.core.gen.emf.ui.popup.actions;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.cmf.occi.core.Extension;
+import org.eclipse.cmf.occi.core.util.OcciRegistry;
+import org.eclipse.cmf.occi.core.gen.emf.ui.Messages;
+
+public class RegisterExtensionAction implements IObjectActionDelegate {
+	private ISelection selection;
+	private Shell shell;
+
+	/**
+	 * Constructor for Action1.
+	 */
+	public RegisterExtensionAction() {
+		super();
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		shell = targetPart.getSite().getShell();
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		if (selection != null) {
+			IFile selectedFile = (IFile) ((IStructuredSelection) selection)
+					.getFirstElement();
+
+			// Use a platform:/resource/ URI
+			URI uri = URI.createPlatformResourceURI(selectedFile.getFullPath().toString(), true);
+
+			ResourceSet rs = new ResourceSetImpl();
+			Resource r = rs.getResource(uri, true);
+
+			Extension extension = (Extension) r.getContents().get(0);
+			OcciRegistry.getInstance().registerExtension(extension.getScheme(),
+					uri.toString());
+			MessageDialog.openInformation(shell,
+					Messages.RegisterExtensionAction_ExtRegistration,
+					Messages.RegisterExtensionAction_RegisteredExtension
+							+ extension.getScheme());
+		}
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		this.selection = selection;
+	}
+
+}
