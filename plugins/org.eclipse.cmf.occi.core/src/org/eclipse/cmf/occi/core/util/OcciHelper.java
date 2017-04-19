ee09f7e98141fba6005454a85c701b6267701167
Philippe Merle <Philippe.merle@inria.fr>
2017-04-19 16:15:09 +0200

    Update file header.

--- plugins/org.eclipse.cmf.occi.core/src/org/eclipse/cmf/occi/core/util/OcciHelper.java
+++ plugins/org.eclipse.cmf.occi.core/src/org/eclipse/cmf/occi/core/util/OcciHelper.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017 Inria
+ * Copyright (c) 2015-17 Inria
  *  
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,6 +7,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
+ * - Philippe Merle <philippe.merle@inria.fr>
  * - Faiez Zalila <faiez.zalila@inria.fr>
  */
 package org.eclipse.cmf.occi.core.util;
