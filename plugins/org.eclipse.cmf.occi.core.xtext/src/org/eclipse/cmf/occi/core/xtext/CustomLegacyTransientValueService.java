d4dce0245511c6d710adf0b74f52208c301060d0
fzalila <faiez.zalila@gmail.com>
2017-06-06 23:07:50 +0200

    add the case of recordField

--- plugins/org.eclipse.cmf.occi.core.xtext/src/org/eclipse/cmf/occi/core/xtext/CustomLegacyTransientValueService.java
+++ plugins/org.eclipse.cmf.occi.core.xtext/src/org/eclipse/cmf/occi/core/xtext/CustomLegacyTransientValueService.java
@@ -2,6 +2,7 @@ package org.eclipse.cmf.occi.core.xtext;
 
 import org.eclipse.cmf.occi.core.Kind;
 import org.eclipse.cmf.occi.core.OCCIPackage;
+import org.eclipse.cmf.occi.core.RecordType;
 import org.eclipse.cmf.occi.core.Type;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
@@ -10,17 +11,20 @@ import org.eclipse.xtext.serializer.sequencer.LegacyTransientValueService;
 import org.eclipse.xtext.serializer.sequencer.ITransientValueService.ValueTransient;
 
 public class CustomLegacyTransientValueService extends LegacyTransientValueService {
-	
+
 	@Override
 	public ValueTransient isValueTransient(EObject semanticObject, EStructuralFeature feature) {
-		if (feature == OCCIPackage.Literals.ATTRIBUTE__MUTABLE && semanticObject.eContainer() instanceof Type) {
-			if ((boolean)semanticObject.eGet(feature)) {
-				return ValueTransient.NO;
+
+		if (feature == OCCIPackage.Literals.ATTRIBUTE__MUTABLE) {
+			if (semanticObject.eContainer() instanceof Type || semanticObject.eContainer() instanceof RecordType) {
+				if ((boolean)semanticObject.eGet(feature)) {
+					return ValueTransient.NO;
+				}
 			}
 		}
 		return super.isValueTransient(semanticObject, feature);
 	}
-	
+
 	@Override
 	protected boolean defaultValueIsSerializeable(EStructuralFeature feature) {
 		if (feature == OCCIPackage.Literals.ATTRIBUTE__MUTABLE) {
