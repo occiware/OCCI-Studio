22e79ba532c5131c9d45d97f9d4dda71dd6da541
fzalila <faiez.zalila@gmail.com>
2017-08-07 18:37:53 +0200

    Add the new Latex documentation generator.

---
+++ plugins/org.eclipse.cmf.occi.core.gen.latex/src/org/eclipse/cmf/occi/core/gen/latex/services/GenUtils.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2017 Inria
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Philippe Merle <philippe.merle@inria.fr>
+ *     Faiez Zalila <faiez.zalila@inria.fr>
+ *******************************************************************************/
+package org.eclipse.cmf.occi.core.gen.latex.services;
+
+import java.util.Date;
+import org.eclipse.cmf.occi.core.Extension;
+
+public class GenUtils
+{
+	/**
+	 * Get the current date.
+	 * @return the current date.
+	 */
+	public String getNow() {
+		return new Date().toString();
+	}
+
+	/**
+	 * Get the file path of an extension.
+	 * @param extension the given extension.
+	 * @return the file path of the given extension.
+	 */
+	public String getFilePath(Extension extension) {
+		return extension.eResource().getURI().toString();
+	}
+	public String getUserName(Extension extension) {
+		return System.getProperty("user.name");
+	}
+}
