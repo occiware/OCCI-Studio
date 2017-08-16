a28e460e4df152e9aba9c5b037f23e6598d1d31d
fzalila <faiez.zalila@gmail.com>
2017-08-16 17:41:51 +0200

    Update properties and copyrights of org.eclipse.cmf.occi.core.design.

--- plugins/org.eclipse.cmf.occi.core.design/src/org/eclipse/cmf/occi/core/design/utils/WizardUtils.java
+++ plugins/org.eclipse.cmf.occi.core.design/src/org/eclipse/cmf/occi/core/design/utils/WizardUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015-2016 Obeo, Inria
+ * Copyright (c) 2015-2017 Obeo, Inria
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,6 +8,7 @@
  * Contributors:
  * - William Piers <william.piers@obeo.fr>
  * - Philippe Merle <philippe.merle@inria.fr>
+ * - Faiez Zalila <faiez.zalila@inria.fr>
  *******************************************************************************/
 package org.eclipse.cmf.occi.core.design.utils;
 
