9f44de8074c12cea78d1600c85c4ea21a22883e5
fzalila <faiez.zalila@gmail.com>
2017-08-16 18:13:33 +0200

    Update the properties and copyrights of org.eclipse.cmf.occi.core.ui.

--- plugins/org.eclipse.cmf.occi.core.ui/src/org/eclipse/cmf/occi/core/ui/popup/actions/Messages.java
+++ plugins/org.eclipse.cmf.occi.core.ui/src/org/eclipse/cmf/occi/core/ui/popup/actions/Messages.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2017 Obeo, Inria
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * 	   William Piers <william.piers@obeo.fr>
+ *     Philippe Merle <philippe.merle@inria.fr>
+ *     Faiez Zalila <faiez.zalila@inria.fr>
+ *******************************************************************************/
 package org.eclipse.cmf.occi.core.ui.popup.actions;
 
 import org.eclipse.osgi.util.NLS;
