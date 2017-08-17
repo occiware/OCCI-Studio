9e407f05ef45c5955e592d73f8dd2a7b4163401c
fzalila <faiez.zalila@gmail.com>
2017-08-17 16:02:51 +0200

    Update the properties and copyrights of URIs Generator.

--- plugins/org.eclipse.cmf.occi.core.gen.uris.ui/src/org/eclipse/cmf/occi/core/gen/uris/ui/common/GenerateAll.java
+++ plugins/org.eclipse.cmf.occi.core.gen.uris.ui/src/org/eclipse/cmf/occi/core/gen/uris/ui/common/GenerateAll.java
@@ -1,12 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2012 Obeo.
+ * Copyright (c) 2015-2017 Obeo, Inria
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *     Obeo - initial API and implementation
+ * 	   William Piers <william.piers@obeo.fr>
+ *     Philippe Merle <philippe.merle@inria.fr>
+ *     Faiez Zalila <faiez.zalila@inria.fr>
  *******************************************************************************/
 package org.eclipse.cmf.occi.core.gen.uris.ui.common;
 
