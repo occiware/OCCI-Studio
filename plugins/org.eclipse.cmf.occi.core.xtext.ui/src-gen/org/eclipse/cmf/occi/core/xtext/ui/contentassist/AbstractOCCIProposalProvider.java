ae10f25756a70669fff06e54fad38223750419a1
fzalila <faiez.zalila@gmail.com>
2017-09-05 15:07:19 +0200

    Regenerate org.eclipse.cmf.occi.core.xtext.ui.

--- plugins/org.eclipse.cmf.occi.core.xtext.ui/src-gen/org/eclipse/cmf/occi/core/xtext/ui/contentassist/AbstractOCCIProposalProvider.java
+++ plugins/org.eclipse.cmf.occi.core.xtext.ui/src-gen/org/eclipse/cmf/occi/core/xtext/ui/contentassist/AbstractOCCIProposalProvider.java
@@ -460,10 +460,10 @@ public abstract class AbstractOCCIProposalProvider extends TerminalsProposalProv
 	public void complete_NameID(EObject model, RuleCall ruleCall, ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
 		// subclasses may override
 	}
-	public void complete_ID_WITHOUT_CARET(EObject model, RuleCall ruleCall, ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
+	public void complete_QUALIFIED_ID(EObject model, RuleCall ruleCall, ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
 		// subclasses may override
 	}
-	public void complete_QUALIFIED_ID(EObject model, RuleCall ruleCall, ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
+	public void complete_ID_WITHOUT_CARET(EObject model, RuleCall ruleCall, ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
 		// subclasses may override
 	}
 	public void complete_KEYWORD(EObject model, RuleCall ruleCall, ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
